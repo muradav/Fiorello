5186c33ff6c4b39c3c27ac2f43acc7b30b9dfe8f
muradav <muradav@code.edu.az>
2022-07-04 16:30:57 +0400

    feat: detail added

--- FrontToBack/FrontToBack/Areas/AdminPanel/Controllers/CategoryController.cs
+++ FrontToBack/FrontToBack/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using FrontToBack.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace FrontToBack.Areas.AdminPanel.Controllers
 {
@@ -35,6 +36,15 @@ namespace FrontToBack.Areas.AdminPanel.Controllers
                 return View();
             }
             return Content($"{category.Name} {category.Desc}");
+        }
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null) return NotFound();
+            Category dbCategory = await _context.Categories.FindAsync(id);
+            if(dbCategory==null) return NotFound();
+
+            return View(dbCategory);
         }
     }
 }
